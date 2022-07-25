0f391cce58b9786af0ede08389fc67ccaffeb687
Subhamsidharth <ssmahapatra1478@gmail.com>
2022-07-25 12:19:50 +0530

    str

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+mongoose.connect('mongodb+srv://RinkiPradhan:moShtmwBC2cEopn2@cluster0.xs93j.mongodb.net/Group70Database', {
+  useNewUrlParser: true
+})
+.then(function(){
+  console.log("Mongodb is connected successfully.");
+})
+.catch(function(err){
+  console.log(err)
+})
+
+app.use('/', route);
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log('Your app is running at port', process.env.PORT || 3000);
+})
