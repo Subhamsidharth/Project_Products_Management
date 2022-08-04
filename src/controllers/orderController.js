626dfbc8593602bf173225ed96dd68ee49b26401
Subhamsidharth <ssmahapatra1478@gmail.com>
2022-08-04 13:20:34 +0530

    route changes

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -12,7 +12,7 @@ const {isValid,isValidObjectId,isValidRequestBody,isBoolean} = require("../valid
 //   - _**On success**_ - Return HTTP status 200. Also return the order document. The response should be a JSON object like [this](#successful-response-structure)
 //   - _**On error**_ - Return a suitable error message with a valid HTTP status code. The response should be a JSON object like [this](#error-response-structure)
 
-module.exports.createOrder = async (req, res)=>{
+const createOrder = async (req, res)=>{
     try {
         const userId = req.params.userId
         let Body = req.body
@@ -73,3 +73,4 @@ module.exports.createOrder = async (req, res)=>{
     }
 }
 
+module.exports={createOrder}
